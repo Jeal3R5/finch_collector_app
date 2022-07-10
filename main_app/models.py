c898973140d58a747b91a4f30edf85951e26bb77
Joan Renfroe <joan.renfroe@gmail.com>
2022-07-09 19:50:39 -0700

    create superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,39 @@
+from audioop import reverse
 from django.db import models
 
 # Create your models here.
+class Germ(models.Model):
+    common_name = models.CharField(max_length=100)
+    germ_name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    mode_of_trans = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.common_name
+
+    # def get_absolute_url(self):
+    #     return reverse("detail", kwargs={'germ_id': self.id})
+
+    
+
+
+
+
+
+
+
+
+
+
+# g = Germ(common_name='COVID-19', germ_name="SARS-CoV-2", type="Virus", mode_of_trans="Contact/Droplet")
+
+
+
+
+
+
+    #   self.common_name = common_name
+    #     self.germ_name = germ_name
+    #     self.type = type
+    #     self.mode_of_trans = mode_of_trans
--- main_app/views.py
+++ main_app/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Germ
 
 
-class Germ:
-    def __init__(self, common_name, germ_name, type, mode_of_trans):
-        self.common_name = common_name
-        self.germ_name = germ_name
-        self.type = type
-        self.mode_of_trans = mode_of_trans
+# class Germ:
+#     def __init__(self, common_name, germ_name, type, mode_of_trans):
+#         self.common_name = common_name
+#         self.germ_name = germ_name
+#         self.type = type
+#         self.mode_of_trans = mode_of_trans
 
-germs = [
-    Germ('MRSA', 'Methicillin-Resistant Staphylococcus aureus', 'Bacteria', 'Contact'),
-    Germ('COVID-19', 'SARS-CoV-2', 'Virus', 'Droplet'),
-    Germ('C Auris', 'Candida auris', 'Fungi', 'Contact'),
-    Germ('Chagas Disease', 'Trypanosoma cruzi', 'Protozoa', 'Vectorborne' )
-]
+# germs = [
+#     Germ('MRSA', 'Methicillin-Resistant Staphylococcus aureus', 'Bacteria', 'Contact'),
+#     Germ('COVID-19', 'SARS-CoV-2', 'Virus', 'Droplet'),
+#     Germ('C Auris', 'Candida auris', 'Fungi', 'Contact'),
+#     Germ('Chagas Disease', 'Trypanosoma cruzi', 'Protozoa', 'Vectorborne' )
+# ]
 
 
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')      #This is like res.send in express
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')     
 
 
 def about(request):
     return render(request, 'about.html')
 
 def germs_index(request):
-    return render(request, 'germs/index.html', { 'germs': germs })
+    germs = Germ.objects.all()
+    return render(request, "germs/index.html", {'germs': germs })
