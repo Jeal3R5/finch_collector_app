a4998c7bb3fbad7720b34c827579859f9b91c4dc
Joan Renfroe <joan.renfroe@gmail.com>
2022-07-09 18:06:52 -0700

    hardcoded germ list

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('germs/', views.germs_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+class Germ:
+    def __init__(self, common_name, germ_name, type, mode_of_trans):
+        self.common_name = common_name
+        self.germ_name = germ_name
+        self.type = type
+        self.mode_of_trans = mode_of_trans
+
+germs = [
+    Germ('MRSA', 'Methicillin-Resistant Staphylococcus aureus', 'Bacteria', 'Contact'),
+    Germ('COVID-19', 'SARS-CoV-2', 'Virus', 'Droplet'),
+    Germ('C Auris', 'Candida auris', 'Fungi', 'Contact'),
+    Germ('Chagas Disease', 'Trypanosoma cruzi', 'Protozoa', 'Vectorborne' )
+]
+
+
+
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')      #This is like res.send in express
 
 
 def about(request):
     return render(request, 'about.html')
+
+def germs_index(request):
+    return render(request, 'germs/index.html', { 'germs': germs })
